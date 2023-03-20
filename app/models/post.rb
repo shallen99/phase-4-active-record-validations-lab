4c4e1baa093553e975311fd30a69dc5029111030
+ <+>
2023-03-20 11:52:00 +0300

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ['Fiction', 'Non-Fiction'] 
+    validate :title_contains_clickbait
+
+    def title_contains_clickbait
+        unless title.present? && (title.include?("Won't Believe") || title.include?("Secret") || title.include?("Guess"))
+        errors.add(:title, "should be sufficiently clickbait-y")
+        end
+    end
+
 end
